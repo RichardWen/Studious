26170dc9e648aa7d7965e8d8d1c2c7c34cf15cea
RichardWen <richardwen01@gmail.com>
2016-08-17 22:14:28 -0700

    last bit of ui fixes

---
+++ public/javascripts/chat_controller.js
@@ -0,0 +1,51 @@
+angular.module('app', ["pubnub.angular.service"])
+ .controller('ChatCtrl', function($scope, Pubnub) {
+   $scope.channel = 'messages-channel';
+   // Generating a random uuid between 1 and 100 using an utility function from the lodash library.
+   Pubnub.init({
+         publish_key: 'pub-c-9c137f5b-1757-47fc-9dc7-33fd883615af',
+         subscribe_key: 'sub-c-a6a3c768-640c-11e6-962a-02ee2ddab7fe',
+         uuid: $scope.uuid
+       });
+    });
+     // Send the messages over PubNub Network
+     $scope.sendMessage = function() {
+        // Don't send an empty message
+        if (!$scope.messageContent || $scope.messageContent === '') {
+             return;
+         }
+         Pubnub.publish({
+             channel: $scope.channel,
+             message: {
+                 content: $scope.messageContent,
+                 sender_uuid: $scope.uuid,
+                 date: new Date()
+             },
+             callback: function(m) {
+                 console.log(m);
+             }
+         });
+         // Reset the messageContent input
+         $scope.messageContent = '';
+
+     }
+     $scope.messages.push(m);
+     $scope.messages = [];
+
+     // Subscribing to the ‘messages-channel’ and trigering the message callback
+     Pubnub.subscribe({
+         channel: $scope.channel,
+         triggerEvents: ['callback']
+     });
+
+     // Listening to the callbacks
+     $scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function (ngEvent, m) {
+         $scope.$apply(function () {
+             $scope.messages.push(m)
+         });
+     });
+
+     // A function to display a nice uniq robot avatar
+     $scope.avatarUrl = function(uuid){
+         return 'http://robohash.org/'+uuid+'?set=set2&bgset=bg2&size=70x70';
+     };
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Get main page */
 router.get('/Comms', function(req, res, next) {
     res.render('mainpage', {title: 'Main'})
 });
 
+/* Get tbd page */
 router.get('/TBD', function(req, res, next) {
     res.render('tobeadded', {title: 'Main'})
 });
