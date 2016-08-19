6d54720e5a95522f3e131acc7dbec003903d09d2
RichardWen <richardwen01@gmail.com>
2016-08-19 12:54:08 -0700

    Blog Post #1

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Get main page */
+/* Get Main page */
 router.get('/Comms', function(req, res, next) {
     res.render('mainpage', {title: 'Main'})
 });
+
+/* Get Blog page */
+router.get('/Blog', function(req, res, next) {
+    res.render('blog', {title: 'Main'})
+});
+
+
 
 /* Get tbd page */
 router.get('/TBD', function(req, res, next) {
