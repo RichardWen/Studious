39409755159c2768d5b6e287380ed433969c422a
RichardWen <richardwen01@gmail.com>
2016-08-23 14:25:40 -0700

    I'm actually dumb

--- public/chatcomponents/services/chat.config.js
+++ public/chatcomponents/services/chat.config.js
@@ -4,7 +4,8 @@ angular.module('app')
     // Please signup to PubNub to use your own keys: https://admin.pubnub.com/
     publish_key: 'pub-c-9c137f5b-1757-47fc-9dc7-33fd883615af',
     subscribe_key: 'sub-c-a6a3c768-640c-11e6-962a-02ee2ddab7fe',
+    ssl: true,
     uuid: currentUser
-    ssl: true
+
   });
 }]);
