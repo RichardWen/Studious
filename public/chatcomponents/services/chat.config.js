411b277d647cfa96a3e5ae1924abb5a7c81c8a8b
RichardWen <richardwen01@gmail.com>
2016-08-23 14:23:49 -0700

    SSL fix

--- public/chatcomponents/services/chat.config.js
+++ public/chatcomponents/services/chat.config.js
@@ -5,5 +5,6 @@ angular.module('app')
     publish_key: 'pub-c-9c137f5b-1757-47fc-9dc7-33fd883615af',
     subscribe_key: 'sub-c-a6a3c768-640c-11e6-962a-02ee2ddab7fe',
     uuid: currentUser
+    ssl: true
   });
 }]);
